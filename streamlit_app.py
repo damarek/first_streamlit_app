b9a3677a44c43db15b87332b69a5cd18db374ce6
damarek <damarek1@gmail.com>
2023-08-02 15:32:19 +0200

    Changed default value to actually the users fruit variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if streamlit.button('Get Fruit Load List'):
 #allow the end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit');")
+         my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('" + new_fruit + "');")
          return "Thanks for adding " + new_fruit
      
 
